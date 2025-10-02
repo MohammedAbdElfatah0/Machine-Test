f776b8f096a1682e73f430a2c837ed48b7dcf698
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-02 16:33:39 +0300

    create  const images

---
+++ lib/core/contents/images/images.dart
@@ -0,0 +1,5 @@
+class Images {
+  static const String logo = 'assets/images/Logo.png';
+  static const String profile = 'assets/images/profile.png';
+  static const String whatsApp = 'assets/images/whatsApp.png';
+}
